c084c9c092fa591d9b7f1b4294fbe76b3b13c0bc
Lengyel László <lengyel.laszlo.lllaci@gmail.com>
2024-10-14 17:41:14 +0200

    Update exercisse_3_order_of_customers.sql
    Version 2

--- SQL/exercisse_3_order_of_customers.sql
+++ SQL/exercisse_3_order_of_customers.sql
@@ -5,13 +5,15 @@ Write a query to display the total number of orders placed by each customer,
 along with their name and the date they joined. Sort the results by the total number of orders in descending order.
 */
 
-SELECT Customers.customer_name AS "Customer's name",
-    Customers.join_date AS "Date they joined",
-    count(Orders.order_id) AS "Count of orders"
+SELECT 
+    Customers.customer_id,
+    Customers.customer_name,
+    Customers.join_date,
+    count(Orders.order_id) AS total_orders
 FROM Customers
 JOIN Orders ON Orders.customer_id = Customers.customer_id
-GROUP BY Customers.customer_name, Customers.join_date
-ORDER BY "Count of orders" DESC;
+GROUP BY Customers.customer_id, Customers.customer_name, Customers.join_date
+ORDER BY total_orders DESC;
 
 /*
 Question 2: Rank Customers by Spending
@@ -22,13 +24,15 @@ Use a **window function** to rank the customers.
 */
 
 SELECT 
-    rank() OVER (PARTITION BY country ORDER BY "Total spending" DESC),
-    *
+    *,
+    rank() OVER (PARTITION BY country ORDER BY total_spent DESC) As spending_ranking_in_country
 FROM
-    (SELECT Customers.customer_name,
+    (SELECT 
+        Customers.customer_id,
+        Customers.customer_name,
         Customers.country,
-        sum(OrderItems.quantity * OrderItems.price) As "Total spending"
+        sum(OrderItems.quantity * OrderItems.price) As total_spent
     FROM customers
     JOIN Orders ON Orders.customer_id = Customers.customer_id
     JOIN OrderItems ON Orderitems.order_id = Orders.order_id
-    GROUP BY Customers.customer_name,Customers.country) Spendings;
+    GROUP BY Customers.customer_id, Customers.customer_name,Customers.country) Spendings;
